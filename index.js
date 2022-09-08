97726a4b23c2674bab969cde0b9d9110266cd215
Kulsum Shaikh <shaikhkulsum233@gmail.com>
2022-09-08 18:19:48 -0500

    commit tree a45149e9e3d20fbfc43ee37efc5ae1e34e719d8c

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(names) {
+    const letters = []
+    for (let i = 0; i < names.length; i++) {
+       letters.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+    }
+    return letters
+}
+
+writeCards(["Guadalupe", "Ollie", "Aki"]);
+
+function countDown() {
+    let counting = 0;
+    while (counting <= 10) {
+      console.log(counting++);
+    }
+}
